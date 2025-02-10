0a041fce155e79edba3962809eef7567387ad30c
Tom Walker <twalkerrcodes@outlook.com>
2025-02-10 14:46:32 -0500

    Add pagination, sorting, filtering and search
    Add pagination, sorting, filtering and search

--- src/Infrastructure/Data/SpecificationEvaluator.cs
+++ src/Infrastructure/Data/SpecificationEvaluator.cs
@@ -28,6 +28,11 @@ public class SpecificationEvaluator<T> where T : BaseEntity
             query = query.Distinct();
         }
 
+        if(spec.IsPagingEnabled)
+        {
+            query = query.Skip(spec.Skip).Take(spec.Take);
+        }
+
         return query;
     }
 
@@ -60,7 +65,11 @@ public class SpecificationEvaluator<T> where T : BaseEntity
         {
             selectQuery = selectQuery?.Distinct();
         }
-
+        
+        if(spec.IsPagingEnabled)
+        {
+             selectQuery =  selectQuery?.Skip(spec.Skip).Take(spec.Take);
+        }
         return selectQuery ?? query.Cast<TResults>();
     }
 }
