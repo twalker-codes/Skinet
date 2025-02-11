7c455e8408b9f9d3c37534c7d3011ebb58b95843
Tom Walker <twalkerrcodes@outlook.com>
2025-02-11 15:40:33 -0500

    Added fetching of products to client
    Added fetching of products to client

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 
 @Component({
@@ -10,6 +13,17 @@ import { HeaderComponent } from "./layout/header/header.component";
   styleUrl: './app.component.scss',
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'http://localhost:5000/api/'
+  private http = inject(HttpClient);
   title = 'Skinet';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+    })
+  }
 }
