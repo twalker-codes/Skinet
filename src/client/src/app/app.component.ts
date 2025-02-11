8b54f42b7a61fc53f8abd4bb235a0e995b9135bc
Tom Walker <twalkerrcodes@outlook.com>
2025-02-11 12:52:12 -0500

    Change css back to scss
    Change css back to scss

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+
 
 @Component({
   selector: 'app-root',
-  //imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'Skinet';
