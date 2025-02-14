b666be053da85ad3f77d592c065f7ba74ac64b1e
Tom Walker <twalkerrcodes@outlook.com>
2025-02-14 12:45:32 -0500

    add client side error handling
    add client side error handling

--- src/client/src/app/layout/header/header.component.ts
+++ src/client/src/app/layout/header/header.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
 import {MatButton} from '@angular/material/button';
 import {MatBadge} from '@angular/material/badge';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
+import { MatProgressBar } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-header',
@@ -13,10 +15,11 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
      MatBadge,
      RouterLink,
      RouterLinkActive,
+     MatProgressBar
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  constructor(public router: Router) {}
+  busyService = inject(BusyService);
 }
