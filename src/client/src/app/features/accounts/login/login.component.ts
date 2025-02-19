fd44fb8f6c48bf47dac05e82fc8cc0320ae6ee25
Tom Walker <twalkerrcodes@outlook.com>
2025-02-19 15:48:24 -0500

    saving work on client account service
    saving work on client account service

--- src/client/src/app/features/accounts/login/login.component.ts
+++ src/client/src/app/features/accounts/login/login.component.ts
@@ -5,7 +5,7 @@ import { MatCard } from '@angular/material/card';
 import { MatInput } from '@angular/material/input';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { AccountService } from '../../../core/services/account.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -24,14 +24,6 @@ export class LoginComponent {
   private fb = inject(FormBuilder);
   private accountService = inject(AccountService);
   private router = inject(Router);
-  private activatedRoute = inject(ActivatedRoute);
-  returnUrl = '/shop';
-
-  
-  constructor() {
-    const url = this.activatedRoute.snapshot.queryParams['returnUrl'];
-    if (url) this.returnUrl = url;
-  }
 
   loginForm = this.fb.group({
     email: [''],
@@ -41,9 +33,10 @@ export class LoginComponent {
   onSubmit() {
     this.accountService.login(this.loginForm.value).subscribe({
       next: () => {
-        this.accountService.getUserInfo().subscribe();
-        this.router.navigateByUrl(this.returnUrl);
+        this.accountService.getUserInfo();
+        this.router.navigateByUrl('/shop');
       }
-    })
+    });
   }
 }
+
